a10d302d74f0e2a46608c8e0a09693710e81e057
Paul Franco <paulfranco@me.com>
2018-11-24 16:08:20 -0800

    Updated FailedToast, InfoToast, SuccessToast classes

--- beautifultoastlibrary/src/main/java/co/paulfran/paulfranco/beautifultoastlibrary/InfoToast.java
+++ beautifultoastlibrary/src/main/java/co/paulfran/paulfranco/beautifultoastlibrary/InfoToast.java
@@ -1,4 +1,28 @@
 package co.paulfran.paulfranco.beautifultoastlibrary;
 
-public class InfoToast {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InfoToast extends LinearLayout {
+
+    public InfoToast(Context context, String message) {
+        super(context);
+        show(context, message);
+    }
+    private void show(Context context, String message) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.info_toast, this, true);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText(message);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+    }
 }
--- beautifultoastlibrary/src/main/java/co/paulfran/paulfranco/beautifultoastlibrary/SuccessToast.java
+++ beautifultoastlibrary/src/main/java/co/paulfran/paulfranco/beautifultoastlibrary/SuccessToast.java
@@ -1,4 +1,29 @@
 package co.paulfran.paulfranco.beautifultoastlibrary;
 
-public class SuccessToast {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SuccessToast extends LinearLayout{
+
+    public SuccessToast(Context context, String message) {
+        super(context);
+        show(context, message);
+    }
+
+    private void show(Context context, String message) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.success_toast, this, true);
+
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText(message);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+    }
 }
